a2934a4f58e27880fbf7edd3931e1da24ca2ab1b
surahutomo.pradana@bukalapak.com <surahutomo.pradana@bukalapak.com>
2023-11-14 05:25:23 +0700

    initial commit

--- fetch-credentials.py
+++ fetch-credentials.py
@@ -2,17 +2,13 @@
 
 import requests
 
-# Replace 'YOUR_JSON_URL' with the actual URL of the JSON data
 json_url = "https://firestore.googleapis.com/v1/projects/berlayar-ai/databases/(default)/documents/credentials/api"
 
-# Fetch JSON data from the URL
 response = requests.get(json_url)
 data = response.json()
 
-# Extract the list of data objects inside the fields object
 fields_data = data.get('fields', {})
 
-# Print the list of data objects
 for key, value in fields_data.items():
     print(f"{key}={value['stringValue']}")
 
